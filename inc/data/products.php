d21379d5f4092d881717274435b4fffb16b5c08e
chalumeau <chalumeauk@gmail.com>
2020-05-27 16:56:42 +0200

    sessions

---
+++ inc/data/products.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$catalog = [
+    46 => [
+        'name' => 'Pecan nuts',
+        'description' => 'Cooked by Penny !',
+        'prix' => 2.80,
+    ],
+    36 => [
+        'name' => 'Chocolate chips',
+        'description' => 'Cooked by Bernadette !',
+        'prix' => 5,
+    ],
+    58 => [
+        'name' => 'Full chocolate cookie',
+        'description' => 'Cooked by Bernadette !',
+        'prix' => 18,
+    ],
+    32 => [
+        'name' => 'M&M\'s&copy; cookies',
+        'description' => 'Cooked by Penny !',
+        'prix' => 12,
+    ],
+];
